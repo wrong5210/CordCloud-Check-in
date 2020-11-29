1ff1db1d3986b7d9e2788ebbc1db1c035c2e95e5
wo1framalpha <37956663+wo1framalpha@users.noreply.github.com>
2020-11-29 16:06:59 +0800

    debug

--- cc_auto_check_in.sh
+++ cc_auto_check_in.sh
@@ -2,6 +2,6 @@
 sed -i 's/SECRET_EMAIL/"${{ secrets.EMAIL }}"/' ./config.py
 sed -i 's/SECRET_PASSWD/"${{ secrets.PASSWD }}"/' ./config.py
 sed -i 's/SECRET_SERVER_CHAN/"${{ secrets.SERVER_CHAN }}"/' ./config.py
-echo ./config.py
+cat ./config.py
 
 python cc_auto_check_in.py
